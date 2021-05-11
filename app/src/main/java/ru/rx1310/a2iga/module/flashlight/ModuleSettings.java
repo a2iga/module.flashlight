6eb9ecd54a059f7a6712e7ed7e00eed91e57d572
rx1310 <rx1310@inbox.ru>
2021-05-11 23:04:50 +0300

    Небольшие правки

--- app/src/main/java/ru/rx1310/a2iga/module/flashlight/ModuleSettings.java
+++ app/src/main/java/ru/rx1310/a2iga/module/flashlight/ModuleSettings.java
@@ -1,5 +1,7 @@
 package ru.rx1310.a2iga.module.flashlight;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -14,13 +16,37 @@ public class ModuleSettings extends PreferenceActivity {
         PreferenceScreen p = getPreferenceManager().createPreferenceScreen(this);
         setPreferenceScreen(p);
 
-        Preference a = new Preference(this);
-        a.setEnabled(false);
-        a.setSummary(R.string.msg_no_settings);
+        Preference p1 = new Preference(this);
+        p1.setEnabled(false);
+        p1.setSummary(R.string.msg_no_settings);
 
+		Preference p2 = new Preference(this);
+		p2.setTitle(R.string.pref_module_dev);
+		p2.setSummary(R.string.module_dev);
+		p2.setKey("MODULE_DEV");
         
-        p.addPreference(a);
+        p.addPreference(p1);
+		p.addPreference(p2);
 
     }
+	
+	public boolean onPreferenceTreeClick(PreferenceScreen ps, Preference p) {
+
+        switch (p.getKey()) {
+
+            case "MODULE_DEV":
+                Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(getString(R.string.module_dev_link)));
+				i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(i);
+                break;
+
+            default: break;
+
+        }
+
+        return super.onPreferenceTreeClick(ps, p);
+
+	}
 
 }
